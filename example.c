20d5c4f72687615563847621619a0bb287eaacb4
root <root@DESKTOP-GNNTH80>
2024-02-29 18:29:24 +0000

    kindly check it

--- example.c
+++ example.c
@@ -35,8 +35,7 @@ PhysicalMemoryFrame physical_memory[FRAME_NUM];
 bool frame_status[FRAME_NUM];
 
 // function definitions
-void initializePageTables(FirstLevelEntry* first_level_page_table);
-void initializeFrameStatus();
+
 
 // initialize entries in the first level page table 
 void initializePageTables(FirstLevelEntry* first_level_page_table) { //takes a pointer to the first level entry array
@@ -51,75 +50,149 @@ void initializeFrameStatus(){
     }
 }
 
-int workloadOne(int rows, int cols){
-    // define matrix 
-    int **matrix;
-    // int matrix[rows][cols];
-    matrix = (int **)malloc(rows * sizeof(int *));
+// int workloadOne(int rows, int cols){
+//     // define matrix 
+//     int **matrix;
+//     // int matrix[rows][cols];
+//     matrix = (int **)malloc(rows * sizeof(int *));
 
-    if (matrix == NULL) {
-        fprintf(stderr, "Memory allocation failed for matrix rows.\n");
-        return -1;  // Return an error code
-    }
+//     if (matrix == NULL) {
+//         fprintf(stderr, "Memory allocation failed for matrix rows.\n");
+//         return -1;  // Return an error code
+//     }
 
-        // Dynamically allocate memory for each row of the matrix
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int *)malloc(cols * sizeof(int));
-        if (matrix[i] == NULL) {
-            fprintf(stderr, "Memory allocation failed for matrix columns.\n");
+//         // Dynamically allocate memory for each row of the matrix
+//     for (int i = 0; i < rows; i++) {
+//         matrix[i] = (int *)malloc(cols * sizeof(int));
+//         if (matrix[i] == NULL) {
+//             fprintf(stderr, "Memory allocation failed for matrix columns.\n");
 
-            // Free previously allocated memory to avoid memory leaks
-            for (int j = 0; j < i; j++) {
-                free(matrix[j]);
-            }
-            free(matrix);
+//             // Free previously allocated memory to avoid memory leaks
+//             for (int j = 0; j < i; j++) {
+//                 free(matrix[j]);
+//             }
+//             free(matrix);
 
-            return -1;  // Return an error code
-        }
-    }
+//             return -1;  // Return an error code
+//         }
+//     }
     
-    // Initialize the matrix with some random values
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int random = rand()  % 11;
-            matrix[i][j] = i * random + j ;
+//     // Initialize the matrix with some random values
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             int random = rand()  % 11;
+//             matrix[i][j] = i * random + j ;
+//         }
+//     }
+
+//     // Print the matrix
+//     printf("Matrix:\n");
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             printf("%d\t", matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     int rand_sum = 0;
+
+//     // calculate and sum row/2 of elements in the matrix
+//     for(int i=0; i < rows/2 ; i++){
+//         int row_rand =  rand() % rows;
+//         int col_rand  = rand() % cols;
+//         rand_sum += matrix[row_rand][col_rand];
+//     }
+
+//     printf("The sum of %d random numbers in the array is %d \n", rows/2, rand_sum);
+
+//     // Free dynamically allocated memory
+//     for (int i = 0; i < rows; i++) {
+//         free(matrix[i]);
+//     }
+//     free(matrix);
+
+//     return 0;
+// }
+
+
+int workloadOne(int rows, int cols, int process_id, FirstLevelEntry *first_level_page_table) {
+    // Allocate pages in the first-level page table
+    for (int i = 0; i < PAGE_NUM; ++i) {
+        if (first_level_page_table[0].second_level_page_table == NULL) {
+            first_level_page_table[0].second_level_page_table = (int *)malloc(PAGE_NUM * sizeof(int));
+
+            for (int j = 0; j < PAGE_NUM; ++j) {
+                first_level_page_table[0].second_level_page_table[j] = -1;
+            }
+
+            // Allocate frames for the process
+            for (int j = 0; j < FRAME_NUM; ++j) {
+                if (!frame_status[j]) {
+                    frame_status[j] = true;
+                    first_level_page_table[0].second_level_page_table[j] = j;
+                }
+            }
+
+            break; // Break once pages are allocated
         }
     }
 
-    // Print the matrix
-    printf("Matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d\t", matrix[i][j]);
-        }
-        printf("\n");
+    // Access data using a virtual address
+    int virtual_address =  process_id * PAGE_SIZE;
+    int physical_address = translateVirtualToPhysical(virtual_address, 0, first_level_page_table);
+
+    if (physical_address != -1) {
+        char data = physical_memory[physical_address / PAGE_SIZE].data[physical_address % PAGE_SIZE];
+        printf("Data at virtual address %d in process %d is %d\n", virtual_address, 0, data);
     }
 
-    int rand_sum = 0;
-
-    // calculate and sum row/2 of elements in the matrix
-    for(int i=0; i < rows/2 ; i++){
-        int row_rand =  rand() % rows;
-        int col_rand  = rand() % cols;
-        rand_sum += matrix[row_rand][col_rand];
-    }
-
-    printf("The sum of %d random numbers in the array is %d \n", rows/2, rand_sum);
-
-    // Free dynamically allocated memory
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
+    free(first_level_page_table[process_id].second_level_page_table);
+    first_level_page_table[process_id].second_level_page_table = NULL;
 
     return 0;
 }
+
+
+
+
+
+
+int translateVirtualToPhysical(int virtual_address, int process_id, FirstLevelEntry* first_level_page_table) {
+    int virtual_page_number = virtual_address / PAGE_SIZE;
+
+    // Check if the page table is allocated for the process
+    if (first_level_page_table[process_id].second_level_page_table != NULL) {
+        // Get the frame number from the second-level page table
+        int frame_number = first_level_page_table[process_id].second_level_page_table[virtual_page_number];
+
+        // Check if the frame is valid
+        if (frame_status[frame_number]) {
+            int offset = virtual_address % PAGE_SIZE;
+            int physical_address = frame_number * PAGE_SIZE + offset;
+            return physical_address;
+        }
+    }
+
+    printf("Page fault for virtual address %d in process %d\n", virtual_address, process_id);
+    return -1;
+}
+
+
+
+
+
+
+
 
 
 int main(){
     // right now  i want to test that for a process i am able to divide it into pages and allocate 
     // also track statistics 
 
+
+    FirstLevelEntry first_level_page_table[PAGE_NUM];
+    initializePageTables(first_level_page_table);
+    initializeFrameStatus();
     pid_t pid = fork();
 
     if (pid == -1) {
@@ -132,7 +205,7 @@ int main(){
         // This is the child process
         int rows = 5;  // Set the desired number of rows
         int cols = 5;  // Set the desired number of columns
-        int result = workloadOne(rows, cols);
+        int result = workloadOne(rows, cols, pid, first_level_page_table);
 
         // Exit the child process
         exit(result);
